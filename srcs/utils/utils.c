bfaa23b0be5d6b63f187c7916cc8b3a4fbdf9e56
jeseco@live.ca <jeseco@live.ca>
2022-10-25 14:47:51 -0400

    fix utils/get_current_time

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeseco <jeseco@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jcourtem <jcourtem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:41:16 by jcourtem          #+#    #+#             */
-/*   Updated: 2022/10/20 12:21:06 by jeseco           ###   ########.fr       */
+/*   Updated: 2022/10/25 14:46:37 by jcourtem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ time_t	get_current_time(void)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return ((tv.tv_sec * 1000) + (tv.tv_sec / 1000));
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
 int	exit_clean(t_philosophers *philosopher)
